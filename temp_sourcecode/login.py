8d9864e33cf5d50593d80d595a3731063e86e87e
Souheil CHELFOUH <trollfot@gmail.com>
2013-07-26 15:20:57 +0200

    Added base logic in sso

--- temp_sourcecode/login.py
+++ temp_sourcecode/login.py
@@ -1,4 +1,5 @@
-from webob import Response
+# -*- coding: utf-8 -*-
+
 from webob.exc import HTTPFound
 from webob.dec import wsgify
 import datetime, time, base64
@@ -7,48 +8,130 @@ import os
 from urllib import quote
 from M2Crypto import RSA, EVP
 from cStringIO import StringIO
+from paste.urlmap import URLMap
+from dolmen.view import View as BaseView
+from dolmen.template import TALTemplate
+from cromlech.webob import Response, Request
 
 
 iv = os.urandom(16)
 
 
-def bauth(val):
-    def encrypt(data, key):
-        # Zero padding
-        if len(data) % 16 != 0:
-            data += '\0' * (16 - len(data) % 16);
-            buffer = StringIO()
-            cipher = EVP.Cipher('aes_128_cbc', key=key, iv=iv, op=1)
-            cipher.set_padding(0)
-            buffer.write(cipher.update(str(data)))
-            buffer.write(cipher.final())
-            data = iv + buffer.getvalue()
-            return data
-    return encrypt(val, 'mKaqGWwAVNnthL6J')
+login_template = TALTemplate('login.pt')
+timeout_template = TALTemplate('timeout.pt')
+unauthorized_template = TALTemplate('unauthorized.pt')
 
 
-@wsgify
-def application(request):
+class View(BaseView):
+    responseFactory = Response
 
-    if request.GET.get('timeout'):
-        return "Timeout"
-    if request.GET.get('unauth'):
-        return "You're not authorized"
+    def __init__(self, environ, request, template):
+        self.context = environ
+        self.request = request
+        self.template = template
 
-    path = os.path.dirname(__file__)
-    pkey = os.path.join(path, 'pkey')
-    privkey = RSA.load_key(pkey)
 
-    login = "admin"
-    password = "admin"
-    val = base64.encodestring(bauth('%s:%s' % (login, password)))
-    #val = base64.encodestring('%s:%s' % (login, password))
-    validtime = datetime.datetime.now() + datetime.timedelta(hours=1)
-    validuntil = int(time.mktime(validtime.timetuple()))
-    ticket = auth_pubtkt.create_ticket(
-        privkey, login, validuntil, tokens=['su'], extra_fields=(('bauth', val),))
+class LoginForm(BaseView):
+    responseFactory = Response
 
-    res = HTTPFound(location="http://gatekeeper.novareto.de")
-    res.set_cookie('auth_pubtkt', quote(ticket), path='/',
-                   domain='novareto.de', secure=False)
-    return res
+    def __init__(self, context, request, values):
+        self.context = context
+        self.request = request
+        self.values = values
+
+    def namespace(self):
+        ns = dict(
+            view=self,
+            title=_(u'Something'),
+            user_field='username',
+            pass_field='password',
+            button='form-button')
+        ns.update(self.values)
+        return ns
+
+
+def base_authentication(login, passwd):
+    return login == "admin" and passwd == "admin"
+
+
+class Login(object):
+
+    def __init__(self, auth=base_authentication):
+        self.authenticate = auth
+
+    def logged_in_response(self, login, password, camefrom=None):
+        path = os.path.dirname(__file__)
+        pkey = os.path.join(path, 'pkey')
+        privkey = RSA.load_key(pkey)
+
+        val = base64.encodestring(bauth('%s:%s' % (login, password)))
+        validtime = datetime.datetime.now() + datetime.timedelta(hours=1)
+        validuntil = int(time.mktime(validtime.timetuple()))
+        ticket = auth_pubtkt.create_ticket(
+            privkey, login, validuntil, tokens=['su'],
+            extra_fields=(('bauth', val),))
+
+        res = HTTPFound(location="http://gatekeeper.novareto.de")
+        res.set_cookie('auth_pubtkt', quote(ticket), path='/',
+                       domain='novareto.de', secure=False)
+        return res
+
+    def __call__(self, environ, start_response):
+        user = None
+        message = "Please login"
+        request = Request(environ)
+
+        if user is not None:
+            # User is already logged in
+            camefrom = request.GET.get('camefrom', '/')
+            return request.get_response(HTTPFound(location=camefrom))
+
+        username = request.POST.get('username', '')
+        if 'POST' == request.environ['REQUEST_METHOD']:
+            ###
+            # User is currently posting the login form
+            ###
+            camefrom = request.POST.get('camefrom', '/')
+            password = request.POST.get('password', '')
+
+            user = self.authenticate(username, password)
+
+            if user is not None:
+                return self.logged_in_response(username, password, camefrom)
+            else:
+                message = self.failed_message
+        else:
+            ###
+            # User is not logged in and is not submitting the form.
+            ###
+            camefrom = request.GET.get('camefrom', '/')
+
+        # default : we render the form
+        values = {"username": username,
+                  "camefrom": camefrom,
+                  "message": message}
+        form = LoginForm(self, request, **values)
+        name = request.path.split('/')[-1]
+        form.__name__ = form.__component_name__ = name
+        form.__parent__ = self
+        return form()
+
+
+def timeout(environ, start_response):
+    request = Request(environ)
+    response = View(environ, request, timeout_template)()
+    return response(environ, start_response)
+
+
+def unauthorized(environ, start_response):
+    request = Request(environ)
+    response = View(environ, request, unauthorized_template)()
+    return response(environ, start_response)
+
+
+def application(environ, start_response):
+    mapper = URLMap()
+    mapper['/'] = logger
+    mapper['/unauthorized'] = unauthorized
+    mapper['/timeout'] = timeout
+    return not_found(environ, start_response)
