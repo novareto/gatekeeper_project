78b1961ad38ed2fbfba8be979f2b4df3951bca59
Souheil Chelfou <dolmen@TRAP.(none)>
2013-07-26 15:59:00 +0200

    Minimal Setup

---
+++ src/gatekeeper/src/gatekeeper/app.py
@@ -0,0 +1,8 @@
+import uvclight
+from zope.interface import Interface
+
+class Index(uvclight.View):
+    uvclight.context(Interface)
+
+    def render(self):
+        return u"HALLO WELT FROM uVCLIht"
